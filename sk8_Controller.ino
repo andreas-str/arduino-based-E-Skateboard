485b6fd5f7798e98b8190c7bb2102580996a409f
Andreas <andrew48@users.noreply.github.com>
2018-03-08 09:23:18 +0100

    Safety updates @ Analog calibration

--- sk8_Controller.ino
+++ sk8_Controller.ino
@@ -8,20 +8,20 @@
 Servo esc;
 
 int lightpin = 3; //pin where lights FET connect
-int batlevelpin = A4; //analog pin that measuers battery voltage
+int batlevelpin = A2; //analog pin that measuers battery voltage
 int btbt = 13; //bluetooth AT mode button
 int escPin = 9; //esc pin out
 int throttleChangeDelay = 25; //small delay after changing the esc signal
 bool safe = true; //boolean that will not allow the board to run on low power
 int curve = 1100; //curve variable for a variable esc signal input
-int batlevel = 0; // internal battery analog reading
+int batlevel = 0; // internal battery analog reading(0-1023)
 bool timeToSend = true; //bool to control when to send the battery status to the controller
 unsigned long previousMillis = 0; //variable for a delay without using delay
 const long interval = 12000; //how often to send the battery level back
-float voltageLow = 0.00;
-float voltageHigh = 0.00;
-float r1 = 80200.00;
-float r2 = 11800.00;
+float voltageLow = 0.00; //voltage reading of battery using internal reference(0-5v)
+float voltageHigh = 0.00; //converted voltage to real voltage levels
+float r1 = 80200.00; //R1 in the voltage division
+float r2 = 11800.00; //R2 in the voltage division
 
 void setup() {
   esc.attach(escPin); //start the esc output
@@ -38,14 +38,28 @@ void loop() {
   //read battery voltage
   batlevel = analogRead(batlevelpin);
   //read serial if safe, convert data to an int, and pass it to 2 functions
-  if (Serial1.available() > 0 && safe == true) {
-    int throttle = normalizeThrottle( Serial1.parseInt() );
-    changeThrottle(throttle);
-    Lights(throttle);
+  if (Serial1.available() > 0) {
+    if (safe == true) {
+      int throttle = normalizeThrottle( Serial1.parseInt() );
+      changeThrottle(throttle);
+      Lights(throttle);
+    }
   } else {
     curve = 1100;
     esc.writeMicroseconds(curve);
   }
+  //if out of battery while riding, slowlly deaccelerate to 0
+  if (safe == false) {
+    if (curve > 1102) {
+      curve -= 7;
+      esc.writeMicroseconds(curve);
+      delay(throttleChangeDelay);
+    }else{
+      curve = 1100;
+      esc.writeMicroseconds(curve);
+    }
+  }
+
   //delayed function for when to send the battery level to the controller. The battery status is updated every 10 seconds
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
@@ -56,20 +70,23 @@ void loop() {
     }
   }
   //analog voltage(0-1023) to real voltage (0-5v)
-  voltageLow = (sensorValue * 4.62) / 1024.00; //calibrate voltage reference here 
-  voltageHigh = voltageLow / (r2/(r1+r2)); //convert voltage to 0 - <20 v
+  voltageLow = (batlevel * 5.31) / 1024.00; //calibrate voltage reference here
+  voltageHigh = voltageLow / (r2 / (r1 + r2)); //convert voltage to 0 - <20 v
   //depending on the voltage, send the apropriate command to the controller
   if (timeToSend == true) {
-    if (voltageHigh >= 19.80) {                         //Battery between 100-70%
+    if (voltageHigh >= 19.90) {                         //Battery between 100-70%
       Serial1.write(49);
+      safe = true;
     }
-    if (voltageHigh < 19.80 && voltageHigh >= 18.25) {  //Battery between 70-40%
+    if (voltageHigh < 19.90 && voltageHigh >= 19.10) {  //Battery between 70-40%
       Serial1.write(50);
+      safe = true;
     }
-    if (voltageHigh < 18.25 && voltageHigh >= 17.30) {  //Battery between 40-10%
+    if (voltageHigh < 19.10 && voltageHigh >= 18.55) {  //Battery between 40-10%
       Serial1.write(51);
+      safe = true;
     }
-    if (voltageHigh < 17.30) {                          //Battery below 10%
+    if (voltageHigh < 18.55) {                          //Battery below 10%
       Serial1.write(52);
       safe = false;
     }
